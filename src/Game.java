b2f779d54e21987789bff9cac282e30e4b61e0ee
graceokeefe <graceokeefe@students.berkeley.net>
2019-03-28 14:15:18 -0700

    done

--- src/Game.java
+++ src/Game.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class Game {
 
-    public ArrayList<Card> oppCards = new ArrayList<Card>();
-    public int oppSum;
-
     public ArrayList<Card> playerCards = new ArrayList<Card>();
     public int playerSum;
+
+    public ArrayList<Card> oppCards = new ArrayList<Card>();
+    public int oppSum;
 
     public int deckCardCount;
 
